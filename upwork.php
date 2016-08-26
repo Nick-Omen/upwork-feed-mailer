63bbe66416ae647f1bdc592e4fe977b3a0bc7e7d
Nick-Omen <nikita.omen666@gmail.com>
2016-08-26 18:21:11 +0600

    Added checks in code, added error messages

--- upwork.php
+++ upwork.php
@@ -4,12 +4,15 @@ require_once('./mailer.php');
 
 if (!$configs['query']) return;
 
+
 function start_watching()
 {
+    $GLOBALS['configs'] = get_configs();
+    
     global $mysqli, $configs;
 
-    $results = $mysqli->query("SELECT `value` FROM `config` WHERE `key`='cron_active'");
     $cronActive = false;
+    $results = $mysqli->query("SELECT `value` FROM `config` WHERE `key`='cron_active'");
     if (gettype($results) == 'object' && $results->fetch_row()[0] == '1') $cronActive = true;
     if (!$cronActive) return;
 
@@ -24,37 +27,48 @@ function start_watching()
     curl_close($curl_handle);
     
     if(!$xmlUpworkJobs) {
-        $xmlUpworkJobs = file_get_contents($url);    
+        $xmlUpworkJobs = file_get_contents($url);
     }
 
     $upworkJobs = simplexml_load_string($xmlUpworkJobs);
     // Parse upwork jobs
     $parsedUpworkJobs = parse_jobs_from_upwork($upworkJobs);
-    if (count($parsedUpworkJobs) > 0) {
+
+    if ($parsedUpworkJobs) {
         // Latest date
         $latestDate = get_latest_date($parsedUpworkJobs);
         $viewedJobsIds = get_viewed_jobs_by_date($latestDate);
         $notViewedJobs = filter_unviewed_jobs($viewedJobsIds, $parsedUpworkJobs);
-        insert_new_jobs($notViewedJobs);
-        $jobsSendIds = send_email($notViewedJobs);
-        if (strlen($jobsSendIds) > 1) {
-            set_jobs_status_send($jobsSendIds);
+        if ($notViewedJobs) {
+            insert_new_jobs($notViewedJobs);
+            $jobsSendIds = send_email($notViewedJobs);
+            if ($jobsSendIds) {
+                set_jobs_status_send($jobsSendIds);
+            }
         }
     }
     $now = new DateTime();
-    $date = "{$now->date} {$now->timezone}";
+    $date = date(DATE_ATOM, $now->getTimestamp());
     $mysqli->query("UPDATE `config` SET `value` ='{$date}' WHERE `key`='latest_update'");
-    sleep((int)$configs['sleep_seconds']);
-    start_watching();
+//    sleep((int)$configs['sleep_seconds']);
+//    start_watching();
 }
 
 // Functions
 
 /**
  * Send email with new jobs.
+ * @param $jobs
+ * @return string
+ * @throws phpmailerException
  */
 function send_email($jobs)
 {
+    if(gettype($jobs) != 'array' && count($jobs) == 0) {
+        error_log('No jobs to send in mail.');
+        return '';
+    }
+
     global $mail;
 
     $str = '';
@@ -64,21 +78,23 @@ function send_email($jobs)
     foreach ($jobs as $id => $j) {
         $s = isset($j['parsed_summary']['Skills']) ? $j['parsed_summary']['Skills'] : ' - ';
         $b = isset($j['parsed_summary']['Budget']) ? $j['parsed_summary']['Budget'] : ' - ';
-        $mailBody .= "<tr><th width='50%' align='left'>{$j['title']}<th><td width='32%' align='left'>{$s}</td><td width='10%' align='right'>{$b}</td><td width='8%' align='right'><a target='_blank' href='{$j['link']}'>link</a></td></tr>";
-        $str .= (string)$id.',';
+        $d = isset($j['updated']) ? $j['updated'] : ' - ';
+        $mailBody .= "<tr><th width='40%' align='left'>{$j['title']}<th><td width='27%' align='left'>{$s}</td><td width='15%' align='left'>{$d}</td><td width='10%' align='right'>{$b}</td><td width='8%' align='right'><a target='_blank' href='{$j['link']}'>link</a></td></tr>";
+        $str .= "'".(string)$id."',";
     }
     $mailBody .= '</table>';
     $mail->Body = $mailBody;
+    $str = substr($str, 0, -1);
 
-    if($mail->send()) return substr($str, 0, -1);
+    if($mail->send()) return $str;
 
     return '';
 }
 
 /**
  * Find and get job ID from link.
- * @param $link - string
- * @return job id - string
+ * @param $link
+ * @return string
  */
 function get_job_id($link)
 {
@@ -88,9 +104,8 @@ function get_job_id($link)
 
 /**
  * Parse summary of the requested body.
- * @param $summary - string
- * @param $key
- * @return job summary - array
+ * @param $summary
+ * @return array
  */
 function parse_summary($summary)
 {
@@ -112,13 +127,18 @@ function parse_summary($summary)
 
 /**
  * Parse summary of the requested body.
- * @param $jobs - object
- * @return parsed jobs - array
+ * @param $jobs
+ * @return array
  */
 function parse_jobs_from_upwork($jobs)
 {
-    if (gettype($jobs) != 'object') trigger_error('Cant read jobs.');
     $parsedJobs = array();
+
+    if (gettype($jobs) != 'object' || count($jobs) == 0) {
+        error_log('No jobs from XML was parsed.');
+        return $parsedJobs;
+    };
+
     for ($i=0; $i < count($jobs->entry); $i++) {
         $jobId = get_job_id((string)$jobs->entry[$i]->id);
         $parsedJobs[$jobId]['title'] = (string)$jobs->entry[$i]->title;
@@ -128,11 +148,21 @@ function parse_jobs_from_upwork($jobs)
         $summary = (string)$jobs->entry[$i]->summary;
         $parsedJobs[$jobId]['parsed_summary'] = parse_summary($summary);
     }
+
     return $parsedJobs;
 }
 
+/**
+ * Insert new jobs in db.
+ * @param $jobs
+ * @return bool|mysqli_result
+ */
 function insert_new_jobs($jobs)
 {
+    if(gettype($jobs) != 'array' && count($jobs) == 0) {
+        error_log('No jobs to insert');
+        return false;
+    }
     global $mysqli;
 
     $sqlInsertNewJobs = "INSERT IGNORE INTO `viewed_jobs`(`job_id`, `date`) VALUES ";
@@ -142,16 +172,20 @@ function insert_new_jobs($jobs)
     }
     $sqlInsertNewJobs = substr($sqlInsertNewJobs, 0, -1);
 
-    return $results = $mysqli->query($sqlInsertNewJobs);
+    return $mysqli->query($sqlInsertNewJobs);
 }
 
 /**
  * Get latest date from array of jobs.
- * @param $jobs - array
- * @return latest date - string
+ * @param $jobs
+ * @return string
  */
 function get_latest_date($jobs)
 {
+    if(gettype($jobs) != 'array' || count($jobs) == 0) {
+        error_log('Jobs are not an array or jobs count is 0.');
+        return '';
+    }
     $lDate = new DateTime();
     foreach ($jobs as $j) {
         $tmpDateTime = new DateTime($j['updated']);
@@ -163,16 +197,20 @@ function get_latest_date($jobs)
 }
 
 /**
- * Get most recent date to get request from base.
- * @param $date - string
- * @return ids oj jobs - array
+ * Request jobs from db by date.
+ * @param $date
+ * @return array
  */
 function get_viewed_jobs_by_date($date)
 {
+    $jobIds = array();
+    if (gettype($date) != 'string' || strlen($date) == 0) {
+        error_log('Date is not a string or its empty.');
+        return $jobIds;
+    }
     global $mysqli;
 
     $sql = "SELECT `job_id` FROM `viewed_jobs` WHERE `date` >='{$date}' AND `send`=0";
-    $jobIds = array();
     if ($results = $mysqli->query($sql)) {
         foreach ($results as $r) {
             $jobIds[] = $r['job_id'];
@@ -183,23 +221,34 @@ function get_viewed_jobs_by_date($date)
 
 /**
  * Filter unviewed jobs by ids from db
- * @param $ids - array
- * @param $jobs - array
- * @return unviewed jobs - array
+ * @param $ids
+ * @param $jobs
+ * @return array
  */
 function filter_unviewed_jobs($ids, $jobs)
 {
+    if(gettype($ids) != 'array' || gettype($jobs) != 'array' || count($jobs) == 0) {
+        error_log('No jobs to filter.');
+        return array();
+    }
     foreach($jobs as $k => $v) {
         if (in_array((string)$k, $ids)) unset($jobs[$k]);
     }
     return $jobs;
 }
 
+/**
+ * Set job status to send.
+ * @param $ids
+ * @return bool|mysqli_result
+ */
 function set_jobs_status_send($ids)
 {
+    if (gettype($ids) != 'string' || strlen($ids) == 0) {
+        error_log('No ids to update');
+        return false;
+    }
     global $mysqli;
-
-    if (gettype($ids) != 'string') return false;
 
     $sql = "UPDATE `viewed_jobs` SET `send` = 1 WHERE `job_id` IN ({$ids})";
     return $mysqli->query($sql);
