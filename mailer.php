029b9345449b4729b928a9902e24dca67f08e905
Nick-Omen <nikita.omen666@gmail.com>
2016-08-28 12:15:54 +0600

    Mailer fixes

--- mailer.php
+++ mailer.php
@@ -4,7 +4,7 @@ require_once(dirname(__FILE__) . '/vendor/phpmailer/phpmailer/PHPMailerAutoload.
 
 $mail = new PHPMailer;
 $mail->isSMTP();
-$mail->Host = 'smtp.gmail.com';
+$mail->Host = gethostbyname('smtp.gmail.com');
 $mail->SMTPAuth = true;
 $mail->Username = SMTP_USER;
 $mail->Password = SMTP_PASSWORD;
@@ -12,7 +12,7 @@ $mail->SMTPSecure = 'tls';
 $mail->Port = 587;
 
 $sendTo = explode(',', $configs['emails']);
-$mail->setFrom('smtp.omen666@gmail.com', 'Upwork notification');
+$mail->setFrom('smtp.omen666@gmail.com', 'Upwork personal notification');
 
 if(count($sendTo) > 0) {
     foreach ($sendTo as $email) {
@@ -22,4 +22,4 @@ if(count($sendTo) > 0) {
 
 $mail->isHTML(true);
 
-$mail->Subject = "New jobs on upwork by key '{$configs['query']}'!";
+$mail->Subject = "New jobs on Upwork found'!";
